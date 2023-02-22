3a44b8a31d8b01c44d600cd1f63bfe4f7ac14d43
dev37 <dev37@DESKTOP-KN57A6F>
2023-02-23 00:55:25 +0700

    .

--- src/Cms.Api/Services/SequenceService.cs
+++ src/Cms.Api/Services/SequenceService.cs
@@ -2,10 +2,7 @@
 using Dapper;
 using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
-using System;
-using System.Collections.Generic;
 using System.Data;
-using System.Linq;
 using System.Threading.Tasks;
 
 
